39cbc812320d5852e1846ab38a209feb9f0e372a
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2018-10-05 15:19:11 -0300

    Actualiza el fuente para un nuevo ejemplo

--- k8s.go
+++ k8s.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
+func getServerIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	return conn.LocalAddr().(*net.UDPAddr).IP.String()
+}
+
 func playHome(w http.ResponseWriter, r *http.Request) {
 	html := `<!DOCTYPE html><html><body><center>
-		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.0.png">
-		<h1 style="color:blue">Practicando con Kubernetes</h1>
-		<h3 style="color:blue">Versión: twogghub/web:1.0</h3>	
+		<img src="https://raw.githubusercontent.com/twogg-git/k8s-workshop/master/src/1.2.png">
+		<h1 style="color:green">Practicando con Kubernetes</h1>
+		<h2 style="color:blue">IP Servidor:` + getServerIP() + `</h2>
+		<h3 style="color:blue">Versión: twogghub/k8s-workshop:1.2-yaml</h3>
 		</center></body></html>`
 	fmt.Fprintf(w, html)
 }
